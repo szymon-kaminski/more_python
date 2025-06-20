021b48a56b152f1c4e78065952087776b4416c26
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-06-20 10:55:56 +0200

    Add examples demonstrating usage of re.findall

--- Biblioteka standardowa M7L06/wyrazenia_regex.py
+++ Biblioteka standardowa M7L06/wyrazenia_regex.py
@@ -7,7 +7,7 @@ tekst = "Dziś jest piątek"
 match = re.search(r"piątek", tekst)
 if match:
     print("Znaleziono:", match.group())
-##########################################################
+#################################################
 print("\nPrzykład 2")
 import re
 
@@ -18,3 +18,23 @@ dopasowanie = re.search(wzorzec, tekst)
 
 if dopasowanie:
     print("Znalezion:", dopasowanie.group())
+
+####################################################
+####################################################
+print("Przykład re.findall")
+print("Przykład 1")
+
+import re
+
+tekst = "Ma 2 jabłka, 3 gruszki i  4 śliwki"
+cyfry = re.findall(r"\d", tekst)
+print(cyfry)
+##############################################
+print("\nPrzykład 2")
+
+import re
+
+tekst = "Moje numery: 777-555-423, 123-789-012"
+wzorzec = r"\d{3}-\d{3}-\d{3}"
+wyniki = re.findall(wzorzec, tekst)
+print(wyniki)
