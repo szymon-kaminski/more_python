193fdf8ddcf857f8238ef2c196f161383f253199
Szymon Kaminski <szymon.kaminski46@gmail.com>
2025-06-26 14:55:21 +0200

    Fix unit tests for divide() to pass correct arguments and expected results

--- Testing_M08/test_divide_integers.py
+++ Testing_M08/test_divide_integers.py
@@ -2,16 +2,16 @@ from divide_integers import divide
 
 
 def test_case1():
-    assert divide(10 / 5) == 2
+    assert divide(10, 5) == 2
 
 
 def test_case2():
-    assert divide(10 / 0) == "Not defined"
+    assert divide(10, 0) == "Not defined"
 
 
 def test_case3():
-    assert divide(10 / 3) == 3
+    assert divide(10, 3) == 3
 
 
 def test_case4():
-    assert divide(None, 3) == 'Numerator/denominator value is None'
+    assert divide(None, 3) == 'Numerator/denominator value is None.'
